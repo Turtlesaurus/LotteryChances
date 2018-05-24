93f76ed0e29db8877bcdfced932f2546e89726a6
Turtlesaurus <hockey197@msn.com>
2018-05-24 09:50:25 -0500

    -Multithreaded lottery pulls

--- app/src/main/java/com/example/chris/lotterychances/LotteryChancesActivity.java
+++ app/src/main/java/com/example/chris/lotterychances/LotteryChancesActivity.java
@@ -14,6 +14,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DecimalFormat;
 
+
 public class LotteryChancesActivity extends Activity {
     //create global textviews
     TextView mResp;
@@ -22,6 +23,9 @@ public class LotteryChancesActivity extends Activity {
     TextView mP;
     TextView pP;
     TextView lP;
+    getLottery mGL = new getLottery(this);
+    getLottery pGL = new getLottery(this);
+    getLottery lGL = new getLottery(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,111 +39,29 @@ public class LotteryChancesActivity extends Activity {
         pP = (TextView) findViewById(R.id.pProb);
         lP = (TextView) findViewById(R.id.lProb);
 
-        getLottery gl = new getLottery(this);
-        gl.execute();
+        //getLottery gl = new getLottery(this);
+        mGL.runNumbers(2, .000000003304961888, 1.75, "https://www.lotto.net/mega-millions/numbers");
+        pGL.runNumbers(2, .000000003422297813, 1.68, "https://www.lotto.net/powerball/numbers");
+        lGL.runNumbers(1, .00000004911948413, .74, "https://www.lotto.net/illinois-lotto/numbers");
+        //gl.execute();
     }
 
     //update on screen text
-    public void updateTextView(int mJack, int pJack, int lJack, double megaMillions, double powerball, double lotto){
+    public void updateTextView(){
         //update displayed jackpot after adding commas
-        mResp.setText("$" + addCommas(mJack));
-        pResp.setText("$" + addCommas(pJack));
-        lResp.setText("$" + addCommas(lJack));
+        mResp.setText("$" + addCommas(mGL.getJackpot()));
+        pResp.setText("$" + addCommas(pGL.getJackpot()));
+        lResp.setText("$" + addCommas(lGL.getJackpot()));
 
         //update displayed ticket value to two decimal places
-        mP.setText("Ticket value: $" + String.format("%.2f", megaMillions));
-        pP.setText("Ticket value: $" + String.format("%.2f", powerball));
-        lP.setText("Ticket value: $" + String.format("%.2f", lotto));
+        mP.setText("Ticket value: $" + String.format("%.2f", mGL.getProbValue()));
+        pP.setText("Ticket value: $" + String.format("%.2f", pGL.getProbValue()));
+        lP.setText("Ticket value: $" + String.format("%.2f", lGL.getProbValue()));
     }
 
     //makes jackpot amounts pretty
     public String addCommas (int value){
         DecimalFormat formatter = new DecimalFormat("#,###,###,###");
         return formatter.format(value);
-    }
-
-    private class getLottery extends AsyncTask<URL, Integer, Double> {
-
-        private LotteryChancesActivity lca;
-        int pJackpot = 0;
-        int mJackpot = 0;
-        int lJackpot = 0;
-        double powerball = 0;
-        double megaMillions = 0;
-        double lotto = 0;
-
-        public getLottery(LotteryChancesActivity pass){
-            lca = pass;
-        }
-
-        protected Double doInBackground(URL... urls) {
-
-            try {
-                pJackpot = readFromWeb("https://www.lotto.net/powerball/numbers");
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            try {
-                mJackpot = readFromWeb("https://www.lotto.net/mega-millions/numbers");
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            try {
-                lJackpot = readFromWeb("https://www.lotto.net/illinois-lotto/numbers");
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-            //calculate ticket value based on jackpot, odds, and ticket cost
-            powerball = (((pJackpot - 2) * (.000000003422297813)) - 1.68) + 2;
-            megaMillions = (((mJackpot - 2) * (.000000003304961888)) - 1.75) + 2;
-            lotto = (((lJackpot - 1) * (.00000004911948413)) - .74) + 1;
-
-            return null;
-        }
-
-        //send jackpot and ticket values back to view for update
-        protected void onPostExecute(Double nothing){
-            lca.updateTextView(mJackpot, pJackpot, lJackpot, megaMillions, powerball, lotto);
-        }
-    }
-
-    //pulls jackpot amount from given lotto.net game page
-    public static int readFromWeb(String webURL) throws IOException {
-
-        URL url = new URL(webURL);
-        InputStream is =  url.openStream();
-        String jackpotValue = "";
-        String jackpotString = " 0,";
-
-        try( BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.contains("Next Estimated Jackpot")) {
-                    jackpotString = line.substring((line.indexOf("$") + 1), line.indexOf("</span>"));
-                }
-            }
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-            throw new MalformedURLException("URL is malformed!!");
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            throw new IOException();
-        }
-
-        for(int i=0; i<jackpotString.length(); i++) {
-            if(Character.isDigit(jackpotString.charAt(i))){
-                jackpotValue += jackpotString.charAt(i);
-            }else if(jackpotString.charAt(i) == 'M'){
-                i = jackpotString.length();
-                jackpotValue += "000000";
-            }
-        }
-        return Integer.parseInt(jackpotValue);
     }
 }
